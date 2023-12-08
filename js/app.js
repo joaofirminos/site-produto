8074c79fe078c6f3160b728759be7fcaf61ea874
joaofirminos <152315825+joaofirminos@users.noreply.github.com>
2023-12-08 00:48:29 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,31 @@
+let totalGeral;
+limpar();
+
+function adicionar() {
+    let produto = document.getElementById('produto').value;
+    let quantidade = document.getElementById('quantidade').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
+
+    //calcular o preço e o subtotal
+    let preco = quantidade * valorUnitario;
+    
+    //adicionar no carrinho
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span>${nomeProduto} <span class="texto-azul">R$${preco}</span>
+  </section>`;
+    //atualizar valor total
+    
+    totalGeral = totalGeral + preco;
+    let valorTotal = document.getElementById('valor-total');
+    valorTotal.textContent = `R$${totalGeral }`;
+    document.getElementById('quantidade').value = 0;
+}
+
+function limpar(){
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').innerHTML = 'R$ 0';
+
+}
